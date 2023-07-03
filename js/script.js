c56c1c26d48e54186c4122539063f2191a747c72
RenanDevFelipe <renan3301@outlook.com>
2023-07-03 08:35:56 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const MenuBurger = document.querySelector(".menu-burger"),
+    MenuPrincipal = document.querySelector(".menu-principal");
+
+    MenuBurger.addEventListener("click", () =>{
+        MenuPrincipal.classList.toggle("open-menu");
+
+        MenuBurger.classList.toggle("lines");
+    });
+
+// DEIXAR FIXO COM SCROLL //
+
+window.addEventListener("scroll", function (){
+    var NavBar = document.querySelector(".nav-bar-principal");
+    var scrollTop = NavBar.scrollTop || document.documentElement.scrollTop; 
+    
+    if (scrollTop > 250){
+        NavBar.classList.add("nav-fixed-top");
+    }else{
+        NavBar.classList.remove("nav-fixed-top");
+    }
+})
